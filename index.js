768278714e33258ace2d3c8d64025f4725c7722a
llambda <xxgsoftware@gmail.com>
2015-05-10 14:54:23 -0500

    use callbacks mostly to compare perf

--- index.js
+++ index.js
@@ -6,95 +6,76 @@ module.exports = function (connect) {
   var Store = (connect.session) ? connect.session.Store : connect.Store;
 
   function RethinkStore(options) {
+    var self = this;
 
     options = options || {};
     options.clientOptions = options.clientOptions || {};
-    Store.call(this, options); // Inherit from Store
+    Store.call(self, options); // Inherit from Store
 
-    this.browserSessionsMaxAge = options.browserSessionsMaxAge || 86400000; // 1 day
-    this.table = options.table || 'session';
+    self.browserSessionsMaxAge = options.browserSessionsMaxAge || 86400000; // 1 day
+    self.table = options.table || 'session';
     
-    this.connection = r.connect(options.clientOptions)
-    .tap(function (conn) {
-      this.emit('connect');
+    r.connect(options.clientOptions, function (err, conn) {
+      self.conn = conn;
+      self.emit('connect');
 
       r.table('session').indexStatus('expires').run(conn)
       .catch(function (err) {
-        return r.table(this.table).indexCreate('expires').run(conn);
-      }.bind(this))
+        return r.table(self.table).indexCreate('expires').run(conn);
+      });
 
-      this.clearInterval = setInterval(function () {
+      self.clearInterval = setInterval(function () {
         var now = Date.now();
-        r.table(this.table)
+        r.table(self.table)
         .between(0, now, {index: 'expires'})
         .delete()
-        .run(conn)
+        .run(self.conn)
         .tap(function (result) {
           debug('DELETED EXPIRED %j', result);
         }).unref();
-      }.bind(this), options.flushOldSessIntvl || 60000);
-    }.bind(this));
+      }, options.flushOldSessIntvl || 60000);
+    });
   }
 
   RethinkStore.prototype = new Store();
 
   RethinkStore.prototype.get = function (sid, fn) {
-    debug('GETTING "%s" ...', sid);
-    return this.connection.then(function (conn) {
-      return r.table(this.table)
-      .get(sid)
-      .run(conn)
-      .tap(function (data) {
-        debug('GOT %j', data);
-        data = data ? JSON.parse(data.session) : null;
-        fn(null, data);
-      })
-      .catch(function (err) {
-        fn(err);
-        throw err;
-      });
-    }.bind(this))
+    var self = this;
+    r.table(self.table)
+    .get(sid)
+    .run(self.conn)
+    .tap(function (data) {
+      debug('GOT %j', data);
+      data = data ? JSON.parse(data.session) : null;
+      fn(null, data);
+    })
+    .catch(function (err) {
+      fn(err);
+      throw err;
+    });
   };
 
   RethinkStore.prototype.set = function (sid, sess, fn) {
+    var self = this;
+
     var sessionToStore = {
       id: sid,
-      expires: Date.now() + (sess.cookie.originalMaxAge || this.browserSessionsMaxAge),
+      expires: Date.now() + (sess.cookie.originalMaxAge || self.browserSessionsMaxAge),
       session: JSON.stringify(sess)
     };
-    debug('SETTING "%j" ...', sessionToStore);
-    return this.connection.then(function (conn) {
-      return r.table(this.table)
-      .insert(sessionToStore, {
-        conflict: 'update'
-      })
-      .run(conn)
-      .tap(function (data) {
-        debug('SET %j', data);
-        fn();
-      })
-      .catch(function (err) {
-        fn(err);
-        throw err;
-      });
-    }.bind(this));
+    r.table(self.table)
+    .insert(sessionToStore, {
+      conflict: 'update'
+    })
+    .run(self.conn, fn);
   }
 
   RethinkStore.prototype.destroy = function (sid, fn) {
-    debug('DELETING "%s" ...', sid);
-    return this.connection.then(function (conn) {
-      return r.table(this.table)
-      .get(sid)
-      .delete()
-      .tap(function (data) {
-        debug('DELETED %j', data);
-        fn();
-      })
-      .catch(function (err) {
-        fn(err);
-        throw err;
-      });
-    }.bind(this));
+    var self = this;
+    return r.table(self.table)
+    .get(sid)
+    .delete()
+    .run(self.conn, fn);
   }
 
   return RethinkStore;
